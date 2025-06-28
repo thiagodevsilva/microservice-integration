6d97fecd8992c6272047ad8b76d31ad1e8c59a3e
Thiago Silva <thiagows72@gmail.com>
2025-06-27 22:05:45 -0300

    test: ajusta teste do AppController para microserviço TCP com ping

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,21 @@
+// src/app.controller.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appController = module.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('ping', () => {
+    it('should return "pong"', () => {
+      expect(appController.ping()).toBe('pong');
     });
   });
 });
